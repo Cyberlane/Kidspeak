162fa61f75d30c146ac779f16c457a91f6d24a28
Cyberlane <dogbertuk2000@gmail.com>
2017-07-27 19:55:08 +0200

    polyfill to fix window.prompt

--- server/electron-app.js
+++ server/electron-app.js
@@ -1,5 +1,7 @@
-import { app, BrowserWindow } from 'electron';
+/* eslint no-param-reassign: 0 */
+import { app, BrowserWindow, ipcMain } from 'electron';
 import log from 'electron-log';
+import path from 'path';
 
 log.info('App started');
 let mainWindow = null;
@@ -13,10 +15,18 @@ const init = () =>
 
     app.on('ready', () => {
       log.info('Creating blank browser window');
-      mainWindow = new BrowserWindow({ show: false });
+      mainWindow = new BrowserWindow({
+        show: false,
+        webPreferences: {
+          preload: path.join(__dirname, '/polyfill.js'),
+        },
+      });
       mainWindow.loadURL(`file://${__dirname}/../views/loading.html`);
       mainWindow.maximize();
       mainWindow.show();
+      if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools();
+      }
       resolve();
     });
   });
@@ -24,6 +34,37 @@ const init = () =>
 const redirect = (url) => {
   mainWindow.loadURL(url);
 };
+
+let promptResponse = null;
+ipcMain.on('prompt', (eventRet, args) => {
+  promptResponse = null;
+  const promptWindow = new BrowserWindow({
+    width: 200,
+    height: 100,
+    show: false,
+    resizable: false,
+    movable: false,
+    alwaysOnTop: true,
+    frame: false,
+  });
+  const value = args.value || '';
+  const html = `
+    <label for="val">${args.title}</label>
+    <input id="val" value="${value}" autofocus />
+    <button onClick="require('electron').ipcRenderer.send('prompt-response', document.getElementById('val').value);window.close()">Ok</button>
+    <button onClick="window.close()">Cancel</button>
+    <style>body{font-family: sans-serif;} button{float:right;margin-left:10px;} label,input{margin-bottom:10px;width:100%;display:block;}</style>
+  `;
+  promptWindow.loadURL(`data:text/html,${html}`);
+  promptWindow.show();
+  promptWindow.on('closed', () => {
+    eventRet.returnValue = promptResponse;
+  });
+});
+
+ipcMain.on('prompt-response', (event, args) => {
+  promptResponse = args === '' ? null : args;
+});
 
 export {
   init,
