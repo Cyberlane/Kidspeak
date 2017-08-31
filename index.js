d9fc05ec9af340c4fbeddb9c25045926c629859b
Cyberlane <dogbertuk2000@gmail.com>
2017-08-31 19:58:25 +0200

    added watch mode for dev

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import log from 'electron-log';
+import path from 'path';
 import './server';
+
+const [watch] = process.argv.slice(-1);
+if (watch === '-watch') {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  });
+}
+
